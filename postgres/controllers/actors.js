f4fa3c29c1a2e24ac87e3e1645baf5c97eda1709
vietdoox <tuanvietdo.88@gmail.com>
2016-06-05 11:38:08 +0200

    update SC2 and SC3

--- postgres/controllers/actors.js
+++ postgres/controllers/actors.js
@@ -20,161 +20,96 @@ exports.index = function(req,res) {
     res.jsonp({
         message: "Movie API - MongoDB"
     });
-}
-
-/**
- * Create
- */
-exports.create = function(req, res) {
-    var actor = new Actor(req.body.actor);
-    actor.save(function(err) {
-        if(err) res.json({data: null, success: false});
-        else res.json({
-            data: {
-                actor: actor,
-            },
-            success: true
-        });
-    });
 };
 
 /**
- * Read
- */
-exports.read = function(req, res) {
-    var keyword = req.params.actorId;
+ *  SC2 - Detailed actors information
+ *
+ * */
 
-    Actor.findById(req.params.actorId, function(err, actor){
-        if(err || !actor) res.json({data: null, success: false});
-        else res.json({
-            keyword: keyword,
-            data: {
-                actor: actor
-            },
-            success: true
-        });
-    });
-};
-
-/**
- * Update
- */
-exports.update = function(req, res) {
-    Actor.findByIdAndUpdate(req.params.actorId,  { $set: req.body.actor }, function(err, actor){
-        if(err || !actor) res.json({data: null, success: false});
-        else res.json({
-            data: {
-                actor: actor,
-            },
-            success: true});
-    });
-};
-
-/**
- * List
- */
-exports.list = function(req, res) {
-    Actor.find({}, function(err, actors){
-        if(err || !actors) res.json({data: null, success: false});
-        else res.json({
-            data: {
-                actors: actors
-            },
-            success: true
-        });
-    });
-};
-
-/**
- * Search
- * SC2: Detailed actor information
- */
-exports.search = function(req, res) {
+exports.search = function (req, res){
     var keyword = req.params.keyword;
+    var queryString;
 
-    var query;
-
-    // Differentiate between ObjectId and query
-    if( keyword.length >= 12) {
-        query = Actor.find({ $or: [
-            { _id  : new ObjectId(keyword) },
-            { "name.first" : new RegExp(keyword, 'i')},
-            { "name.last" : new RegExp(keyword, 'i')},
-            { aka_names: { "$in" : [new RegExp(keyword, 'i')] }}
-        ]});
-
-    } else {
-        query = Actor.find({ $or: [
-            { "name.first" : new RegExp(keyword, 'i')},
-            { "name.last" : new RegExp(keyword, 'i')},
-            { aka_names: { "$in" : [new RegExp(keyword, 'i')] }}
-        ]});
-    }
-    // Sort movies result from the first
-    query.populate({path: 'movies', options: { sort: { 'year': 1 } } });
-    query.exec(function(err, actors){
-        if(err || !actors ) res.json({data: null, success: false});
-        else {
-
-            actors = _.map(actors, function(num){
-                num.movies_by_year = _.groupBy(num.movies, function(nim){ return nim.year; });
-                return num;
-            });
-
-            res.json({
-                keyword: keyword,
-                data: {
-                    actors: actors
-                },
-                success: true
-            });
+    if (!isNaN(keyword)){
+        //queryString = "SELECT * FROM movies WHERE (idmovies = '"+keyword+"' OR title LIKE '"+keyword+"')";
+        queryString = "SELECT distinct actors.*, aka_names.name AS aka_name, movies.title, movies.year FROM actors join aka_names on actors.idactors = aka_names.idactors" +
+            " join acted_in on actors.idactors = acted_in.idactors"+
+            " join movies on acted_in.idmovies = movies.idmovies"+
+            " WHERE (actors.idactors = '"+keyword+"' OR actors.fname LIKE '"+keyword+"' OR actors.lname LIKE '"+keyword+"')";
+    }else if(keyword.indexOf(" ")>-1){
+        // Could be firstname + lastname
+        var arr = keyword.split(" ");
+        if ((arr[0].length >0)&&(arr[0].length >0)){
+            // firstname and lastname
+            queryString = "SELECT distinct actors.*, aka_names.name AS aka_name, movies.title, movies.year FROM actors join aka_names on actors.idactors = aka_names.idactors" +
+                " join acted_in on actors.idactors = acted_in.idactors"+
+                " join movies on acted_in.idmovies = movies.idmovies"+
+                " WHERE (actors.fname LIKE '"+arr[0]+"' AND actors.lname LIKE '"+arr[1]+"')OR(actors.fname LIKE '"+arr[1]+"' AND actors.lname LIKE '"+arr[0]+"')";
         }
+    }
+    else
+    {
+        //queryString = "SELECT * FROM movies WHERE movies.title LIKE '"+keyword+"'";
+        //SELECT actors.*, aka_names.name AS aka_name FROM actors full join aka_names on actors.idactors = aka_names.idactors WHERE (actors.fname LIKE 'Lucienne' OR actors.lname LIKE 'Lucienne')
+        //queryString = "SELECT actors.*, aka_names.name AS aka_name FROM actors  full join aka_names on actors.idactors = aka_names.idactors WHERE actors.fname LIKE '"+keyword+"' OR actors.lname LIKE '"+keyword+"'";
+        queryString = "SELECT distinct actors.*, aka_names.name AS aka_name, movies.title, movies.year FROM actors join aka_names on actors.idactors = aka_names.idactors" +
+            " join acted_in on actors.idactors = acted_in.idactors"+
+            " join movies on acted_in.idmovies = movies.idmovies"+
+            " WHERE actors.fname LIKE '"+keyword+"' OR actors.lname LIKE '"+keyword+"'";
+    }
+
+    sequelize.query(queryString).spread(function(results, metadata) {
+        //sequelize.query("select distinct * from movies join acted_in on movies.idmovies = acted_in.idmovies where movies.idmovies = 2354").spread(function(results, metadata) {
+        res.json(results);
+        // Results will be an empty array and metadata will contain the number of affected rows.
     });
 };
 
+
 /**
- * Search
- * SC3: Short Actor Statistics
- */
-exports.stats = function(req, res) {
+ *  SC3 - Short actors statics
+ *
+ * */
+
+exports.stats = function (req, res){
     var keyword = req.params.keyword;
+    var queryString;
 
-    var query;
-
-    // Differentiate between ObjectId and query
-    if( keyword.length >= 12) {
-        query = Actor.find({ $or: [
-            { _id  : new ObjectId(keyword) },
-            { "name.first" : new RegExp(keyword, 'i')},
-            { "name.last" : new RegExp(keyword, 'i')},
-            { aka_names: { "$in" : [new RegExp(keyword, 'i')] }}
-        ]});
-
-    } else {
-        query = Actor.find({ $or: [
-            { "name.first" : new RegExp(keyword, 'i')},
-            { "name.last" : new RegExp(keyword, 'i')},
-            { aka_names: { "$in" : [new RegExp(keyword, 'i')] }}
-        ]});
-    }
-    query.populate({path: 'movies', options: { sort: { 'year': 1 } } });
-    query.exec(function(err, actors){
-        if(err || !actors ) res.json({data: null, success: false});
-        else {
-            var astats = _.map(actors, function(num){
-                return {
-                    name: num.name.full,
-                    num_movies: num.movies.length
-                };
-            });
-
-            res.json({
-                keyword: keyword,
-                data: {
-                    actors: astats
-                },
-                success: true
-            });
+    if (!isNaN(keyword)){
+        //queryString = "SELECT * FROM movies WHERE (idmovies = '"+keyword+"' OR title LIKE '"+keyword+"')";
+        queryString = "SELECT distinct actors.idactors, actors.fname,actors.lname, count(movies.idmovies) AS Movie_count FROM actors join aka_names on actors.idactors = aka_names.idactors" +
+            " join acted_in on actors.idactors = acted_in.idactors"+
+            " join movies on acted_in.idmovies = movies.idmovies"+
+            " WHERE (actors.idactors = '"+keyword+"' OR actors.fname LIKE '"+keyword+"' OR actors.lname LIKE '"+keyword+"')"+
+            " GRoup by actors.idactors";
+    }else if(keyword.indexOf(" ")>-1){
+        // Could be firstname + lastname
+        var arr = keyword.split(" ");
+        if ((arr[0].length >0)&&(arr[0].length >0)){
+            // firstname and lastname
+            queryString = "SELECT distinct actors.idactors, actors.fname,actors.lname, count(movies.idmovies) AS Movie_count FROM actors join aka_names on actors.idactors = aka_names.idactors" +
+                " join acted_in on actors.idactors = acted_in.idactors"+
+                " join movies on acted_in.idmovies = movies.idmovies"+
+                " WHERE (actors.fname LIKE '"+arr[0]+"' AND actors.lname LIKE '"+arr[1]+"')OR(actors.fname LIKE '"+arr[1]+"' AND actors.lname LIKE '"+arr[0]+"')"+
+                " GRoup by actors.idactors";
         }
+    }
+    else
+    {
+        //queryString = "SELECT * FROM movies WHERE movies.title LIKE '"+keyword+"'";
+        //SELECT actors.*, aka_names.name AS aka_name FROM actors full join aka_names on actors.idactors = aka_names.idactors WHERE (actors.fname LIKE 'Lucienne' OR actors.lname LIKE 'Lucienne')
+        //queryString = "SELECT actors.*, aka_names.name AS aka_name FROM actors  full join aka_names on actors.idactors = aka_names.idactors WHERE actors.fname LIKE '"+keyword+"' OR actors.lname LIKE '"+keyword+"'";
+        queryString = "SELECT distinct actors.idactors, actors.fname,actors.lname, count(movies.idmovies) AS Movie_count FROM actors join aka_names on actors.idactors = aka_names.idactors" +
+            " join acted_in on actors.idactors = acted_in.idactors"+
+            " join movies on acted_in.idmovies = movies.idmovies"+
+            " WHERE actors.fname LIKE '"+keyword+"' OR actors.lname LIKE '"+keyword+"'"+
+            " GRoup by actors.idactors";
+    }
+
+    sequelize.query(queryString).spread(function(results, metadata) {
+        //sequelize.query("select distinct * from movies join acted_in on movies.idmovies = acted_in.idmovies where movies.idmovies = 2354").spread(function(results, metadata) {
+        res.json(results);
+        // Results will be an empty array and metadata will contain the number of affected rows.
     });
 };
