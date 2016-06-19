a45e15dcd028a0d0c582c61d2b5ebd2acc8bed3b
vietdoox <tuanvietdo.88@gmail.com>
2016-06-19 12:56:23 +0200

    Add SC1 SC2 neo4j

--- neo4j/controllers/actors.js
+++ neo4j/controllers/actors.js
@@ -12,7 +12,7 @@ var apoc = require('apoc'),
  */
 exports.index = function(req,res) {
     res.jsonp({
-        message: "Movie API - MongoDB"
+        message: "Movie API - Neo4j"
     });
 }
 
@@ -21,17 +21,17 @@ exports.index = function(req,res) {
  */
 exports.create = function(req, res) {
     /*
-    var actor = new Actor(req.body.actor);
-    actor.save(function(err) {
-        if(err) res.json({data: null, success: false});
-        else res.json({
-            data: {
-                actor: actor,
-            },
-            success: false
-        });
-    });
-    */
+     var actor = new Actor(req.body.actor);
+     actor.save(function(err) {
+     if(err) res.json({data: null, success: false});
+     else res.json({
+     data: {
+     actor: actor,
+     },
+     success: false
+     });
+     });
+     */
 };
 
 /**
@@ -39,16 +39,16 @@ exports.create = function(req, res) {
  */
 exports.read = function(req, res) {
     /*
-    Actor.findById(req.params.actorId, function(err, actor){
-        if(err || !actor) res.json({data: null, success: false});
-        else res.json({
-            data: {
-                actor: actor,
-            },
-            success: true
-        });
-    });
-    */
+     Actor.findById(req.params.actorId, function(err, actor){
+     if(err || !actor) res.json({data: null, success: false});
+     else res.json({
+     data: {
+     actor: actor,
+     },
+     success: true
+     });
+     });
+     */
 };
 
 /**
@@ -56,15 +56,15 @@ exports.read = function(req, res) {
  */
 exports.update = function(req, res) {
     /*
-    Actor.findByIdAndUpdate(req.params.actorId,  { $set: req.body.actor }, function(err, actor){
-        if(err || !actor) res.json({data: null, success: false});
-        else res.json({
-            data: {
-                actor: actor,
-            },
-            success: true});
-    });
-    */
+     Actor.findByIdAndUpdate(req.params.actorId,  { $set: req.body.actor }, function(err, actor){
+     if(err || !actor) res.json({data: null, success: false});
+     else res.json({
+     data: {
+     actor: actor,
+     },
+     success: true});
+     });
+     */
 };
 
 /**
@@ -72,16 +72,16 @@ exports.update = function(req, res) {
  */
 exports.list = function(req, res) {
     /*
-    Actor.find({}, function(err, actors){
-        if(err || !actors) res.json({data: null, success: false});
-        else res.json({
-            data: {
-                actors: actors
-            },
-            success: true
-        });
-    });
-    */
+     Actor.find({}, function(err, actors){
+     if(err || !actors) res.json({data: null, success: false});
+     else res.json({
+     data: {
+     actors: actors
+     },
+     success: true
+     });
+     });
+     */
 };
 
 /**
@@ -89,40 +89,31 @@ exports.list = function(req, res) {
  * SC2: Detailed actor information
  */
 exports.search = function(req, res) {
-    /*
     var keyword = req.params.keyword;
+    var parameter;
 
-    var query;
-
-    if( keyword.length >= 12) {
-        query = Actor.find({ $or: [
-            { _id  : new ObjectId(keyword) },
-            { "name.first" : new RegExp(keyword, 'i')},
-            { "name.last" : new RegExp(keyword, 'i')},
-            { aka_names: { "$in" : [new RegExp(keyword, 'i')] }}
-        ]});
-
-    } else {
-        query = Actor.find({ $or: [
-            { "name.first" : new RegExp(keyword, 'i')},
-            { "name.last" : new RegExp(keyword, 'i')},
-            { aka_names: { "$in" : [new RegExp(keyword, 'i')] }}
-        ]});
+    if (!isNaN(keyword)){
+        parameter = "m.lname =~ '.*"+keyword+".*'" +" OR "+ "m.fname =~ '.*"+keyword+".*'" + " OR m.idactors ="+keyword;
     }
-    query.populate({path: 'movies', options: { sort: { 'year': -1 } } })
-    query.exec(function(err, actors){
-        if(err || !actors ) res.json({data: null, success: false});
-        else {
-            res.json({
-                keyword: keyword,
-                data: {
-                    actors: actors
-                },
-                success: true
-            });
-        }
+    else parameter = "m.lname =~ '.*"+keyword+".*'" +" OR "+ "m.fname =~ '.*"+keyword+".*'";
+    var query = apoc.query( "Match (a:Actor)-[:HAS_NAME]->(aka_name:Aka_names), " +
+        "a-[:ACTED_IN]->(movies:Movies) " +
+        "WHERE "+ parameter +
+        "return a,aka_names,movies group by a")
+
+    query.exec().then(function (result) {
+        res.json({
+            success: true,
+            data: {
+                movie: result
+            }
+        });
+    }, function (err) {
+        res.json({
+            error: err,
+            success: false
+        });
     });
-    */
 };
 
 /**
@@ -131,47 +122,47 @@ exports.search = function(req, res) {
  */
 exports.stats = function(req, res) {
     /*
-    var keyword = req.params.keyword;
+     var keyword = req.params.keyword;
 
-    var query;
+     var query;
 
-    if( keyword.length >= 12) {
-        query = Actor.find({ $or: [
-            { _id  : new ObjectId(keyword) },
-            { "name.first" : new RegExp(keyword, 'i')},
-            { "name.last" : new RegExp(keyword, 'i')},
-            { aka_names: { "$in" : [new RegExp(keyword, 'i')] }}
-        ]});
+     if( keyword.length >= 12) {
+     query = Actor.find({ $or: [
+     { _id  : new ObjectId(keyword) },
+     { "name.first" : new RegExp(keyword, 'i')},
+     { "name.last" : new RegExp(keyword, 'i')},
+     { aka_names: { "$in" : [new RegExp(keyword, 'i')] }}
+     ]});
 
-    } else {
-        query = Actor.find({ $or: [
-            { "name.first" : new RegExp(keyword, 'i')},
-            { "name.last" : new RegExp(keyword, 'i')},
-            { aka_names: { "$in" : [new RegExp(keyword, 'i')] }}
-        ]});
-    }
-    query.populate({path: 'movies', options: { sort: { 'year': -1 } } })
-    query.exec(function(err, actors){
-        if(err || !actors ) res.json({data: null, success: false});
-        else {
-            var astats = _.map(actors, function(num){
+     } else {
+     query = Actor.find({ $or: [
+     { "name.first" : new RegExp(keyword, 'i')},
+     { "name.last" : new RegExp(keyword, 'i')},
+     { aka_names: { "$in" : [new RegExp(keyword, 'i')] }}
+     ]});
+     }
+     query.populate({path: 'movies', options: { sort: { 'year': -1 } } })
+     query.exec(function(err, actors){
+     if(err || !actors ) res.json({data: null, success: false});
+     else {
+     var astats = _.map(actors, function(num){
 
-                return {
-                    name: num.name.full,
-                    num_movies: num.movies.length
-                };
-            });
+     return {
+     name: num.name.full,
+     num_movies: num.movies.length
+     };
+     });
 
-            res.json({
-                keyword: keyword,
-                data: {
-                    actors: astats
-                },
-                success: true
-            });
-        }
-    });
-    */
+     res.json({
+     keyword: keyword,
+     data: {
+     actors: astats
+     },
+     success: true
+     });
+     }
+     });
+     */
 };
 
 
--- neo4j/controllers/movies.js
+++ neo4j/controllers/movies.js
@@ -42,17 +42,27 @@ exports.create = function(req, res) {
  * Read
  */
 exports.read = function(req, res) {
-    /*
-    Movie.findById(req.params.movieId, function(err, movie){
-        if(err || !movie) res.json({data: null, success: false});
-        else res.json({
+    var query = apoc.query( "Match (m:Movie)-[:HAS_AKATITLES]->(aka_title:Aka_titles), " +
+        "m-[:HAS_KEYWORD]->(keyword:Keyword), " +
+        "m-[:HAS_GENRE]->(genre:Genre), " +
+        "m-[:IN_SERIES]->(series:Series), " +
+        "m<-[:ACTED_IN]-(actors:Actors), "+
+        "return a,aka_title,series,actors")
+
+    query.exec().then(function (result) {
+        res.json({
+            success: true,
             data: {
-                movie: movie,
-            },
-            success: true
+                movie: result
+            }
+        });
+    }, function (err) {
+        res.json({
+            error: err,
+            success: false
         });
     });
-    */
+
 };
 
 /**
@@ -60,33 +70,22 @@ exports.read = function(req, res) {
  */
 exports.update = function(req, res) {
     /*
-    Movie.findByIdAndUpdate(req.params.movieId,  { $set: req.body.movie }, function(err, movie){
-        if(err || !movie) res.json({data: null, success: false});
-        else res.json({
-            data: {
-                movie: movie,
-            },
-            success: true});
-    });
-    */
+     Movie.findByIdAndUpdate(req.params.movieId,  { $set: req.body.movie }, function(err, movie){
+     if(err || !movie) res.json({data: null, success: false});
+     else res.json({
+     data: {
+     movie: movie,
+     },
+     success: true});
+     });
+     */
 };
 
 /**
  * List
  */
 exports.list = function(req, res) {
-    /*
-    Movie.find({}, function(err, movies){
-        if(err || !movies) res.json({data: null, success: false});
-        else res.json({
-            data: {
-                movies: movies,
-                movies_by_year: _.groupBy(movies, function(num){ return num.year; })
-            },
-            success: true
-        });
-    });
-    */
+
 };
 
 /**
@@ -94,40 +93,33 @@ exports.list = function(req, res) {
  * SC1: Detailed movie information
  */
 exports.search = function(req, res) {
-    /*
-    // Get variable for url parameter: keyword
     var keyword = req.params.keyword;
+    var parameter;
 
-    var query;
-
-    // To differentiate whether the keyword is ObjectId or word
-    if( keyword.length >= 12) {
-        query = Movie.find({ $or: [
-            { _id  : new ObjectId(keyword) },
-            { title : new RegExp(keyword, 'i')},
-            { aka_titles: { "$elemMatch" : { title: new RegExp(keyword, 'i') }}}
-        ]});
-    } else {
-        query = Movie.find({ $or: [
-            { title : new RegExp(keyword, 'i')},
-            { aka_titles: { "$elemMatch" : { title: new RegExp(keyword, 'i') }}}
-        ]});
+    if (!isNaN(keyword)){
+        parameter = "m.title =~ '.*"+keyword+".*'"+" OR m.idmovies ="+keyword;
     }
+    else parameter = "m.title =~ '.*"+keyword+".*'";
+    var query = apoc.query( "Match (m:Movie {"+parameter+"})-[:HAS_AKATITLES]->(aka_title:Aka_titles), " +
+        "m-[:HAS_KEYWORD]->(keyword:Keyword), " +
+        "m-[:HAS_GENRE]->(genre:Genre), " +
+        "m-[:IN_SERIES]->(series:Series), " +
+        "m<-[:ACTED_IN]-(actors:Actors), "+
+        "return m,aka_title,series,actors group by m")
 
-    query.exec(function(err, movies){
-        if(err || !movies ) res.json({data: null, success: false});
-        else {
-            res.json({
-                keyword: keyword,
-                data: {
-                    movies: movies,
-                    movies_by_year: _.groupBy(movies, function(num){ return num.year; })
-                },
-                success: true
-            });
-        }
+    query.exec().then(function (result) {
+        res.json({
+            success: true,
+            data: {
+                movie: result
+            }
+        });
+    }, function (err) {
+        res.json({
+            error: err,
+            success: false
+        });
     });
-    */
 };
 
 
@@ -139,19 +131,19 @@ exports.search = function(req, res) {
 // TODO: Implement year range filter
 exports.genre = function(req, res) {
     /*
-    Movie.find({ genres: req.params.genre })
-        .sort({year: -1, title: 1})
-        .populate('casts.actor')
-        .exec(function(err, movies){
-        if(err || !movies) res.json({data: null, success: false});
-        else res.json({
-            data: {
-                movies: movies
-            },
-            success: true
-        });
-    });
-    */
+     Movie.find({ genres: req.params.genre })
+     .sort({year: -1, title: 1})
+     .populate('casts.actor')
+     .exec(function(err, movies){
+     if(err || !movies) res.json({data: null, success: false});
+     else res.json({
+     data: {
+     movies: movies
+     },
+     success: true
+     });
+     });
+     */
 };
 
 /**
@@ -162,19 +154,19 @@ exports.genre = function(req, res) {
 // TODO: Implement year range filter
 exports.genre_year = function(req, res) {
     /*
-    Movie.find({ genres: req.params.genre })
-        .sort({year: -1, title: 1})
-        .populate('casts.actor')
-        .exec(function(err, movies){
-            if(err || !movies) res.json({data: null, success: false});
-            else res.json({
-                data: {
-                    movies: movies
-                },
-                success: true
-            });
-        });
-        */
+     Movie.find({ genres: req.params.genre })
+     .sort({year: -1, title: 1})
+     .populate('casts.actor')
+     .exec(function(err, movies){
+     if(err || !movies) res.json({data: null, success: false});
+     else res.json({
+     data: {
+     movies: movies
+     },
+     success: true
+     });
+     });
+     */
 };
 
 
