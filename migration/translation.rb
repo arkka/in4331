7ace27c43885031353d0b3f7725a7da4bcf9096f
Arkka Dhiratara <arkka.d@gmail.com>
2016-06-19 04:11:08 +0200

    Final data migration script for postgres to mongodb

--- migration/translation.rb
+++ migration/translation.rb
@@ -1,47 +1,5 @@
-table "acted_in", :embed_in => :movies, :on => :idmovies  do
-	column "idacted_in", :key, :as => :integer
-	column "idmovies", :key, :as => :integer
-	column "idseries", :key, :as => :integer
-	column "idactors", :key, :as => :integer
-	column "character", :string
-	column "billing_position", :integer
-end
-
-table "actors" do
-	column "idactors", :key, :as => :integer
-	column "lname", :string
-	column "fname", :string
-	column "mname", :string
-	column "gender", :integer
-	column "number", :integer
-end
-
-table "aka_names", :embed_in => :actors, :on => :idactors  do
-	column "idaka_names", :integer
-	column "idactors", :key, :as => :integer
-	column "name", :string
-end
-
-table "aka_titles", :embed_in => :movies, :on => :idmovies  do
-	column "idaka_titles", :key, :as => :integer
-	column "idmovies", :key, :as => :integer
-	column "title", :string
-	column "location", :string
-	column "year", :integer
-end
-
-table "genres" do
-	column "idgenres", :key, :as => :integer
-	column "genre", :string
-end
-
-table "keywords" do
-	column "idkeywords", :key, :as => :integer
-	column "keyword", :string
-end
-
 table "movies" do
-	column "idmovies", :integer
+	column "idmovies", :key
 	column "title", :string
 	column "year", :integer
 	column "number", :integer
@@ -50,23 +8,71 @@ table "movies" do
 	column "language", :string
 end
 
-table "movies_genres", :embed_in => :movies, :on => :idmovies  do
-	column "idmovies_genres", :key, :as => :integer
-	column "idmovies", :key, :as => :integer
-	column "idgenres", :key, :as => :integer
-	column "idseries", :key, :as => :integer
+table "aka_titles", :embed_in => :movies, :on => :idmovies  do
+	column "idaka_titles", :key
+	column "idmovies", :integer, :references => :movies
+	column "title", :string
+	column "location", :string
+	column "year", :integer
 end
 
+# GENRES
+table "movies_genres", :embed_in => :movies, :on => :idmovies  do
+	column "idmovies_genres", :key
+	column "idmovies", :integer, :references => :movies
+	column "idgenres", :integer, :references => :genres
+	column "idseries", :integer
+end
+
+table "genres", :embed_in => :movies_genres, :on => :idgenres do
+	column "idgenres", :key, :as => :integer
+	column "genre", :string
+end
+
+
+# KEYWORDS
+
 table "movies_keywords", :embed_in => :movies, :on => :idmovies  do
-	column "idmovies_keywords", :key, :as => :integer
-	column "idmovies", :key, :as => :integer
-	column "idkeywords", :key, :as => :integer
-	column "idseries", :key, :as => :integer
+	column "idmovies_keywords", :key
+	column "idmovies", :integer, :references => :movies
+	column "idkeywords", :integer, :references => :keywords
+	column "idseries", :integer
+end
+
+table "keywords", :embed_in => :movies_keywords, :on => :idkeywords do
+	column "idkeywords", :key
+	column "keyword", :string
+end
+
+
+table "actors" do
+	column "idactors", :key
+	column "lname", :string
+	column "fname", :string
+	column "mname", :string
+	column "gender", :integer
+	column "number", :integer
+end
+
+
+table "acted_in", :embed_in => :movies, :on => :idmovies  do
+	column "idacted_in", :key
+	column "idmovies", :integer, :references => :movies
+	column "idseries", :integer
+	column "idactors", :integer, :references => :movies
+	column "character", :string
+	column "billing_position", :integer
+end
+
+table "aka_names", :embed_in => :actors, :on => :idactors  do
+	column "idaka_names", :key
+	column "idactors", :integer, :references => :actors
+	column "name", :string
 end
 
 table "series" do
-	column "idseries", :key, :as => :integer
-	column "idmovies", :key, :as => :integer
+	column "idseries", :key
+	column "idmovies", :integer, :references => :movies
 	column "name", :string
 	column "season", :integer
 	column "number", :integer
