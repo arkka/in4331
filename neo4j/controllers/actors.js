f81b03ac25e508634c04d23c08ae103b960b2f92
sukmawicaksana <sukmawicaksana@gmail.com>
2016-06-20 13:41:46 +0200

    SC2 output improvement

--- neo4j/controllers/actors.js
+++ neo4j/controllers/actors.js
@@ -95,16 +95,57 @@ exports.search = function(req, res) {
     else     parameter = "a.lname =~ '.*"+keyword+".*'" +" OR "+ "a.fname =~ '.*"+keyword+".*'";
     
     var query = apoc.query( "Match (a:Actors)-[:HAS_ALIAS]->(aka_names:AKA_NAMES), " +
-        "(a)-[:ACTED_IN]->(movies:Movies) " +
+        "(a)-[ac:ACTED_IN]->(movies:Movies) " +
         "WHERE "+ parameter +
-        " return a,aka_names,movies");
+        " return distinct a,COLLECT(DISTINCT(aka_names.name)),COLLECT(DISTINCT(movies)), COLLECT(DISTINCT(ac))");
 
-    query.exec().then(function (result) {
-        res.json({
-            success: true,
-            data: {
-                actors: result
+    query.exec().then(function (actors) {
+
+        var result = actors[0];
+
+        var out = [];
+
+        for (var i = 0; i < result.data.length; i++){
+
+            var movies = [];
+
+            var full_name = result.data[i].row[0].fname + " " +  result.data[i].row[0].mname + " " + result.data[i].row[0].lname;
+
+            for (var j = 0; j < result.data[i].row[2].length; j++){
+
+                //movies.push({
+                //    idactors: result.data[i].row[1][j].idactors,
+                //    name: name,
+                //    character: result.data[i].row[2][j].character,
+                //    billing_position: result.data[i].row[2][j].billing_position
+                //});
+
+                movies.push(result.data[i].row[2][j]);
+                movies[j].character = result.data[i].row[3][j].character;
+                movies[j].billing_position = result.data[i].row[3][j].billing_position;
             }
+
+            result.data[i].row[0].full_name = full_name;
+
+            result.data[i].row[0].aka_name = result.data[i].row[1];
+            result.data[i].row[0].movies = movies;
+
+            delete result.data[i].row[0].fname;
+            delete result.data[i].row[0].mname;
+            delete result.data[i].row[0].lname;
+
+            result.data[i].row[0].movies_by_year =_.groupBy(result.data[i].row[0].movies, function(num){ return num.year; });
+
+            out.push(result.data[i].row[0]);
+        }
+
+        res.json({
+            keyword: keyword,
+            count: out.length,
+            data: {
+                actors: out
+            },
+            success: true
         });
     }, function (err) {
         res.json({
